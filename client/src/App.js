7166e5786f1c983a142410e345c3b55e7c65de10
Leon Glaser <leon.glaser@gmail.com>
2022-08-16 10:13:52 -0400

    Addproduct.js updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,36 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import User from './pages/User'
-import Register from './pages/Register'
-import axios from 'axios'
-import SignIn from './pages/SignIn'
-import Member from './components/Member'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import User from "./pages/User";
+import Register from "./pages/Register";
+import axios from "axios";
+import SignIn from "./pages/SignIn";
+import Member from "./components/Member";
 
-import { CheckSession } from './services/Auth'
-import React from 'react'
+import { CheckSession } from "./services/Auth";
+import React from "react";
 
-
-import { useState, useEffect } from 'react'
-import RegisterAsRoaster from './components/RegisterAsRoaster'
-
+import { useState, useEffect } from "react";
+import RegisterAsRoaster from "./components/RegisterAsRoaster";
+import AddNewProduct from "./pages/AddProduct";
 
 export const App = () => {
-  const [authenticated, toggleAuthenticated] = useState(false)
-  const [user, setUser] = useState(null)
+  const [authenticated, toggleAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
 
   const checkToken = async () => {
-    const user = await CheckSession()
-    setUser(user)
-    toggleAuthenticated(true)
-  }
+    const user = await CheckSession();
+    setUser(user);
+    toggleAuthenticated(true);
+  };
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
     if (token) {
-      checkToken()
+      checkToken();
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -42,12 +41,22 @@ export const App = () => {
           <Route path="/shop" element={<Shop />} />
           <Route path="/user" element={<User />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/signin" element={<SignIn user={user} setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>}/>
-          <Route path='/profile' element={<Member user={user} />} />
-        </Routes> 
+          <Route
+            path="/signin"
+            element={
+              <SignIn
+                user={user}
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+          <Route path="/profile" element={<Member user={user} />} />
+          <Route path="/user/addproduct" element={<AddNewProduct />} />
+        </Routes>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
