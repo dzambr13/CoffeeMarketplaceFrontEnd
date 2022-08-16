e621a350f6481b4b56f9aa319750f50acbaa8deb
James Jewitt <jamestjewitt@gmail.com>
2022-08-16 10:37:51 -0400

    template for Shop page

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -7,16 +7,18 @@ const Shop = () => {
 
   return (
     <div className="shop">
+      <div className='shop-navigation'>
+        <Link className='shop-links' to="/register">create an account</Link>
+        <Link className='shop-links' to="/login">sign in</Link>
+        <Link className='shop-links' to="/user">profile</Link>
+        <Link className='shop-links' to="/">home</Link>
+      </div>
 
-
-        <Link to="/">Home</Link>
-        <Link to="/user">User</Link>
-        <Link to="/auth">Auth</Link>
-        <div>
-          <Search />
-          {/* <CoffeeCard /> */}
-          <CoffeeDetails />
-        </div>
+      <div>
+        {/* <Search /> */}
+        {/* <CoffeeCard /> */}
+        <CoffeeDetails />
+      </div>
 
 
 
