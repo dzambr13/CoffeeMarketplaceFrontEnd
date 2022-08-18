4dbbb57ba3e2ea0bac9b37f4d3e3c9caedce1c36
dzambr13 <dayvuhz@gmail.com>
2022-08-17 19:36:50 -0700

    pls

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,5 @@
-
-import Axios from "axios";
-export const BASE_URL = "http://localhost:3001";
-
 import Axios from 'axios'
 export const BASE_URL = 'http://localhost:3001'
-
 // export const BASE_URL = "/";
 
 const Client = Axios.create({ baseURL: BASE_URL })
