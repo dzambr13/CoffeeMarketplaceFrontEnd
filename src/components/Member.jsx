da4718c28913e968fadde569bba92e39ef62441c
Leon Glaser <leon.glaser@gmail.com>
2022-08-18 12:13:05 -0400

    start logout feature

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,33 +1,34 @@
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import React from 'react'
 import { useEffect } from 'react'
 import TestElement from '../pages/TestElement'
 import Client from "../services/api";
 
 
-const Member=({user, sellerProducts, setSellerProducts, GetSellerProducts, setProductToUpdate})=>{
+const Member = ({ user, sellerProducts, setSellerProducts, GetSellerProducts, setProductToUpdate, handleLogout }) => {
 
-    const nav=useNavigate()
-    useEffect(() => {GetSellerProducts();},[]);
-    const deleteProduct=async (pd)=>{
-        let res=await Client.delete(`/api/products/delete/${pd}`)
+    const nav = useNavigate()
+    useEffect(() => { GetSellerProducts(); }, []);
+    const deleteProduct = async (pd) => {
+        let res = await Client.delete(`/api/products/delete/${pd}`)
         GetSellerProducts()
     }
-    const navToUpdate=(pk)=>{
+    const navToUpdate = (pk) => {
         setProductToUpdate(pk)
         nav('/update-product')
     }
-    
+
     return (
         <div className="profile-page">
             <div className='profile-links-container'>
                 <Link className='profile-link' to='/shop'>browse</Link>
                 <Link className='profile-link' to='/'>home</Link>
+                <Link className='logout-button' onClick={handleLogout}></Link>
             </div>
             <div className='profile'>
                 <p className='welcome-user'>Welcome, {user.userName} </p>
                 <div className='profile-featured'>
-                    <div className='featured-products'><TestElement 
+                    <div className='featured-products'><TestElement
                         sellerProducts={sellerProducts}
                         setSellerProducts={setSellerProducts}
                         user={user}
@@ -40,23 +41,23 @@ const Member=({user, sellerProducts, setSellerProducts, GetSellerProducts, setPr
                         <p>{user.firstName}</p>
                         <p>{user.lastName}</p>
                         <p>{user.businessName}</p>
-    
+
                     </div>
                     <div className='orders'>Products
-                        {sellerProducts?.map((product)=>(
+                        {sellerProducts?.map((product) => (
                             <div>
                                 <p>{product.name}</p>
                                 <p>{product.price}</p>
                                 <p>{product.description}</p>
                                 <p>{product.id}</p>
-                                <button onClick={(e)=>{deleteProduct(product.id)}}>Delete</button>
-                                <button onClick={(e)=>{navToUpdate(product.id)}}>Update</button>
+                                <button onClick={(e) => { deleteProduct(product.id) }}>Delete</button>
+                                <button onClick={(e) => { navToUpdate(product.id) }}>Update</button>
                             </div>
                         ))}
-                </div>
+                    </div>
 
+                </div>
             </div>
-        </div>
         </div>
     )
 }
