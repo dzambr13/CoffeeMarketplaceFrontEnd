6db8d02e31436242d3842626def8a14089a744a7
Leon Glaser <leon.glaser@gmail.com>
2022-08-18 16:07:54 -0400

    new member component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,31 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 
 const Search = () => {
 
+    const [search, setSearch] = useState("")
+
+    const searchResult = async (e) => {
+        e.preventDefault()
+        console.log(search)
+        let req = { query: search }
+        let res = await axios.get('http://localhost:3001/api/products/search', req)
+        console.log(res)
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        setSearch(e.target.value)
+    }
     return (
         <div>
-
-            <input className="search-bar" type="text"
-                placeholder="Search by texture, brand or size" />
-
+            <form onChange={(e) => {
+                handleSearch(e)
+            }} onSubmit={(e) => { searchResult(e) }}>
+                <input className="search-bar" type="text"
+                    placeholder="Search products" />
+            </form>
         </div>
     )
 }
