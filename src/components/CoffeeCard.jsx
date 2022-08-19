afeda41acdf0bedb02fefd4f76ceb018d7fdabaf
Leon Glaser <leon.glaser@gmail.com>
2022-08-18 23:08:07 -0400

    correct search axios call

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,18 +1,18 @@
 
-const CoffeeCard=({Product, UpdateProduct, DeleteProduct, index})=>{
+const CoffeeCard = ({ Product, UpdateProduct, DeleteProduct, index }) => {
 
 
     return (
         <div className="coffee-card">
 
-            <img src={Product.productImageUrl} alt="CoffePicture"/>
+            <img src={Product.productImageUrl} alt="CoffePicture" />
             <h3> {Product.name} </h3>
             <p>{Product.roasterId}</p>
             <p> {Product.price} </p>
 
-            //Conditionally render based on user logged in, and if product belongs to them; add confirmation before deleting?
-            <button onClick={()=> UpdateProduct(Product, index)}>Edit product</button>
-            <button onClick={()=> DeleteProduct(Product, index)}>Delete product</button>
+            {/* Conditionally render based on user logged in, and if product belongs to them; add confirmation before deleting? */}
+            <button onClick={() => UpdateProduct(Product, index)}>Edit product</button>
+            <button onClick={() => DeleteProduct(Product, index)}>Delete product</button>
         </div>
     )
 }
